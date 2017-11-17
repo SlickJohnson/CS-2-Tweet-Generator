af7a0a6bd8d3b587b5ec2407e01a42a893539229
SlickJohnson <liwa.johnson@gmail.com>
2017-11-16 17:54:49 -0800

    Add documentation and new functions to generate sentences

--- histograms.py
+++ histograms.py
@@ -1,7 +1,12 @@
+"""This module contains two data models related to histograms."""
+import random
+
 
 class Dictogram(dict):
+    """Histogram data model constructed as a dictionary."""
+
     def __init__(self, iterable=None):
-        """Initialize this histogram as a new dict; update with given items"""
+        """Initialize this histogram as a new dict; update with given items."""
         super(Dictogram, self).__init__()
 
         self.types = 0  # the number of distinct item types in this histogram
@@ -11,7 +16,7 @@ class Dictogram(dict):
             self.update(iterable)
 
     def update(self, iterable):
-        """Update this histogram with the items in the given iterable"""
+        """Update this histogram with the items in the given iterable."""
         for item in iterable:
             if item not in self:
                 self[item] = 0
@@ -21,16 +26,36 @@ class Dictogram(dict):
             self.tokens += 1
 
     def count(self, item):
-        """Return the count of the given item in this histogram, or 0"""
+        """Return the count of the given item in this histogram, or 0."""
         if item not in self:
             return 0
 
         return self[item]
 
+    def get_random_word(self):
+        """Return a random word."""
+        random_key = random.sample(self, 1)
+        return random_key[0]
+
+    def get_weighted_random_word(self):
+        """Return a random word using weights."""
+        random_int = random.randint(0, self.tokens - 1)
+        index = 0
+        list_of_keys = list(self)
+
+        for i in range(0, self.types):
+            index += self[list_of_keys[i]]
+
+            if(index > random_int):
+
+                return list_of_keys[i]
+
 
 class Listogram(list):
+    """Histogram data model constructed as a list of lists."""
+
     def __init__(self, iterable=None):
-        """Initialize this histogram as a new list; update with given items"""
+        """Initialize this histogram as a new list; update with given items."""
         super(Listogram, self).__init__()
 
         self.types = 0  # the number of distinct item types in this histogram
@@ -40,7 +65,7 @@ class Listogram(list):
             self.update(iterable)
 
     def update(self, iterable):
-        """Update this histogram with the items in the given iterable"""
+        """Update this histogram with the items in the given iterable."""
         for item in iterable:
             if not self.__contains__(item):
                 self.append([item, 0])
@@ -50,7 +75,7 @@ class Listogram(list):
             self.tokens += 1
 
     def count(self, item):
-        """Return the count of the given item in this histogram, or 0"""
+        """Return the count of the given item in this histogram, or 0."""
         for word, freq in self:
             if item == word:
                 return freq
@@ -58,15 +83,16 @@ class Listogram(list):
         return 0
 
     def __contains__(self, item):
-        """Return True if the given item is in this histogram, or False"""
+        """Return True if the given item is in this histogram, or False."""
         return item in [word for word, freq in self]
 
     def _index(self, target):
-        """Return the index of the (target, count) entry if found, or None"""
+        """Return the index of the (target, count) entry if found, or None."""
         return self.index([target, self.count(target)])
 
 
 def test_histogram(text_list):
+    """Test the functinality of both histogram data models."""
     print('text list:', text_list)
 
     hist_dict = Dictogram(text_list)
@@ -81,7 +107,7 @@ def read_from_file(filename):
     return open(filename, 'r').read().strip().split()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
 
     arguments = sys.argv[1:]  # exclude script name in first argument
